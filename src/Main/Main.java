fd633e497200f3c823323f06f490b415b1173b20
TUF GAMING <TUF GAMING@DESKTOP-KF221QK.lan>
2022-11-18 14:47:28 +0700

    Turn 1

---
+++ src/Main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import View.MainMenu;
+
+/**
+ *
+ * @author TUF GAMING
+ */
+public class Main {
+    public Main(){
+        new MainMenu();
+    }
+    public static void main(String[] args) {
+        new Main();
+    }
+}
